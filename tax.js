c137114adf980c335010c1cb889f2b8543b59678
medreen.wc <medreenwchege@gmail.com>
2025-11-26 03:40:07 +0300

    added style

--- tax.js
+++ tax.js
@@ -84,22 +84,39 @@ function findTaxableIncome(gross, NSSF, NHDF, NHIF){
 let taxable_income = findTaxableIncome(gross, nssf, nhdf, nhif);
 document.getElementById("taxable_income").innerHTML = taxable_income;
 
-function findPaye(tax_inc){
-    let paye
-    let relief = 2400
-    if (tax_inc <= 24000 && tax_inc >= 0){
-        paye = 24000 * 0.1
-    } else if (tax_inc <= 32333 && tax_inc > 24000){
-        paye = (24000 * 0.1) + 8333 * 0.25
-    } else if (tax_inc <= 500000 && tax_inc > 32333){
-        paye = (24000 * 0.1) + (8333 * 0.25) + (467667 * 0.3) 
-    } else if (tax_inc <= 800000 && tax_inc > 500000){
-        paye = (24000 * 0.1) + (8333 * 0.25) + (467667 * 0.3) + 300000 * 0.325
-    } else if (tax_inc > 800000){
-        paye = (24000 * 0.1) + (8333 * 0.25) + (467667 * 0.3) +( 300000 * 0.325) + (800000 * 0.35);
+function findPaye(tax_inc) {
+    let relief = 2400;
+    let paye = 0;
+
+    if (tax_inc <= 24000) {
+        paye = tax_inc * 0.10;
+    } 
+    else if (tax_inc <= 32333) {
+        paye = (24000 * 0.10) +
+               (tax_inc - 24000) * 0.25;
+    } 
+    else if (tax_inc <= 500000) {
+        paye = (24000 * 0.10) +
+               (8333 * 0.25) +
+               (tax_inc - 32333) * 0.30;
+    } 
+    else if (tax_inc <= 800000) {
+        paye = (24000 * 0.10) +
+               (8333 * 0.25) +
+               (467667 * 0.30) +
+               (tax_inc - 500000) * 0.325;
+    } 
+    else {
+        paye = (24000 * 0.10) +
+               (8333 * 0.25) +
+               (467667 * 0.30) +
+               (300000 * 0.325) +
+               (tax_inc - 800000) * 0.35;
     }
-    return paye - relief
+
+    return paye - relief;
 }
+
 
 let paye = findPaye(taxable_income);
 document.getElementById("paye").innerHTML = paye;
